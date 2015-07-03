e41feafe12147cc78dee38e054cdb6f0936386b9
hirohashi <udpip.nnct@gmail.com>
2015-07-03 22:18:49 +0900

    Modified to handle the case there is no irdb.pkl file

--- IrDev/irdb.py
+++ IrDev/irdb.py
@@ -3,25 +3,39 @@ import pickle
 class irdb:
     def __init__(self):
         self.fname_db = "irdb.pkl"
-        fp = open(self.fname_db)
-        self.dat = pickle.load(fp)
-        fp.close()
-
+        try :
+            fp = open(self.fname_db, "r")
+            self.dat = pickle.load(fp)
+            fp.close()
+        except :
+            self.dat = dict()
+    
     def get_dat(self):
         print self.dat
 
     def keys(self):
         return self.dat.keys()
-
+        
+    def update(self):
+        try :
+            fp = open(self.fname_db, "w")
+            pickle.dump(self.dat, fp)
+            fp.close()
+        except :
+            return False
+        return True
+        
     def store(self, key, value):
         self.dat[key] = value
-        fp = open(self.fname_db, "w")
-        pickle.dump(self.dat, fp)
-        fp.close()
-
+        return self.update()
+    
     def get(self, key):
         return self.dat[key]
-
+    
+    def remove(self, key):
+        self.dat[key].remove()
+        return self.update()
+    
 if __name__ == "__main__":
     db = irdb()
     print db.keys()
